23d21ff86b854ad0515a99ff13f7ff957228afeb
Teodor Savin <t.savin@global.leaseweb.com>
2018-02-04 16:12:41 +0100

    helper generated

--- app/Models/Goalscorer.php
+++ app/Models/Goalscorer.php
@@ -4,6 +4,23 @@ namespace WC2018\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * WC2018\Models\Goalscorer
+ *
+ * @property int $id
+ * @property int $match_id
+ * @property int $player_id
+ * @property int|null $minute
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\WC2018\Models\Goalscorer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\WC2018\Models\Goalscorer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\WC2018\Models\Goalscorer whereMatchId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\WC2018\Models\Goalscorer whereMinute($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\WC2018\Models\Goalscorer wherePlayerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\WC2018\Models\Goalscorer whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Goalscorer extends Model
 {
     protected $fillable = [
--- app/Models/Match.php
+++ app/Models/Match.php
@@ -4,6 +4,33 @@ namespace WC2018\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * WC2018\Models\Match
+ *
+ * @property int $id
+ * @property int $first_team_id
+ * @property int $second_team_id
+ * @property int|null $first_team_goals
+ * @property int|null $second_team_goals
+ * @property string|null $result
+ * @property string $start_time
+ * @property string|null $round
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property int $stadium_id
+ * @method static \Illuminate\Database\Eloquent\Builder|\WC2018\Models\Match whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\WC2018\Models\Match whereFirstTeamGoals($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\WC2018\Models\Match whereFirstTeamId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\WC2018\Models\Match whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\WC2018\Models\Match whereResult($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\WC2018\Models\Match whereRound($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\WC2018\Models\Match whereSecondTeamGoals($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\WC2018\Models\Match whereSecondTeamId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\WC2018\Models\Match whereStadiumId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\WC2018\Models\Match whereStartTime($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\WC2018\Models\Match whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Match extends Model
 {
     protected $fillable = [
